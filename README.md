62ea6051b6ace64150c2e1e37b46e11e2d5418bd
unknown <zckpp1992@gmail.com>
2020-05-24 21:37:57 -0400

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## Tic-Tac-Toe in React
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {game: [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']], player: 'X', winner: ''};
+  }
+
+  handleClick  = (e) => {
+    let rowId = e.target.parentNode.getAttribute('data');
+    let colId = e.target.getAttribute('data');
+    let temp = this.state.game;
+    temp[rowId][colId] = this.state.player;
+    // if winning row
+    if (temp[rowId][0] === temp[rowId][1] && temp[rowId][1] === temp[rowId][2] && temp[rowId][2] !== ' ') {
+      this.setState({winner: temp[rowId][2]});
+    }
+    // check winning column
+    if (temp[0][colId] === temp[1][colId] && temp[1][colId] === temp[2][colId] && temp[2][colId] !== ' ') {
+      this.setState({winner: temp[2][colId]});
+    }
+    if (temp[0][0] === temp[1][1] && temp[1][1] === temp[2][2] && temp[2][2] !== ' ') {
+      this.setState({winner: temp[2][colId]});
+    }
+    if (temp[0][2] === temp[1][1] && temp[1][1] === temp[2][0] && temp[2][0] !== ' ') {
+      this.setState({winner: temp[2][colId]});
+    }
+    this.setState({game: temp});
+    // set next player
+    this.state.player === 'X' ? this.setState({player: 'O'}) : this.setState({player: 'X'});
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          My tic-tac-toe
+        </header>
+        {this.state.winner !== '' && <h1>Winner is {this.state.winner}!!</h1>}
+        <h2>Current Player: {this.state.player}</h2>
+        <table className="map">
+          <tbody>
+            {this.state.game.map((row, index) => 
+              <tr className="row" data={index} key={index}>
+                {row.map((col, index) => 
+                  <td className="col" data={index} key={index} onClick={this.handleClick}>{col}</td>
+                )}
+                </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
 }
 
 export default App;
